2e0c8030235f6c361bb6c516f2d6f5f4ac3e1d40
Januario <deibson.a.januario@accenture.com>
2017-12-07 16:55:11 -0300

    inclusão de localização e da input mask

--- web/module.js
+++ web/module.js
@@ -1,2 +1,2 @@
-var app = angular.module('cdm.system.module', ['ui.router','oc.lazyLoad','ui.mask'])
+var app = angular.module('cdm.system.module', ['ui.router','oc.lazyLoad','ui.mask','ui.utils.masks'])
         .constant('API_URL', 'http://localhost:8091/cdmsystem/public/');
